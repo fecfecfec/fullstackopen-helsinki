193e86b360b9b911fe71c82c7ce2ae78786e811e
Francisco Manuel Ferreiro <48454935+fecfecfec@users.noreply.github.com>
2023-07-16 15:55:10 +0000

    Defined the constants globally.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,10 @@
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
 const Header = (props) => {
   return (
     <>
@@ -6,13 +13,24 @@ const Header = (props) => {
   )
 }
 
-
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
         {props.part} {props.exercises}
       </p>
+    </>
+  )
+}
+
+
+const Content = () => {
+
+  return (
+    <>
+      <Part part={[part1]} exercises={[exercises1]} />
+      <Part part={[part2]} exercises={[exercises2]} />
+      <Part part={[part3]} exercises={[exercises3]} />
     </>
   )
 }
@@ -27,19 +45,11 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part={[part1]} exercises={[exercises1]} />
-      <Content part={[part2]} exercises={[exercises2]} />
-      <Content part={[part3]} exercises={[exercises3]} />
+      <Content />
       <Total value={exercises1 + exercises2 + exercises3}/>
     </div>
   )
