8a225f5c4d76ca085cef05a32695c32807630f8f
Francisco M. Ferreiro <fechex@gmail.com>
2023-07-18 22:26:17 -0300

    Unicafe Step6 finished

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,6 +2,47 @@ import { useState } from 'react'
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 const Display = ({ text, value }) => <div>{text}: {value}</div>
+const StatisticsLine = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = good / total * 100
+  if (total === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+  return (
+    <>
+      <table>
+        <StatisticsLine
+          text='Collected Feedback'
+          value={total}
+        />
+        <StatisticsLine
+          text='Good'
+          value={good}
+        />
+        <StatisticsLine
+          text='Neutral'
+          value={neutral}
+        />
+        <StatisticsLine
+          text='Bad'
+          value={bad}
+        />
+        <StatisticsLine
+          text='Average'
+          value={average}
+        />
+        <StatisticsLine
+          text='Positive'
+          value={positive + '%'}
+        />
+      </table>
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -9,27 +50,44 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  // const rateGood = () => setGood(good + 1)
-  const rateGood = () => { 
-    console.log('Good value before', good)
-    setGood(good + 1)
-  }
-  const rateNeutral = () => setNeutral(neutral - 1)
+  const rateGood = () => setGood(good + 1)
+  // const rateGood = () => {
+  //   console.log('Good value before', good)
+  //   setGood(good + 1)
+  // }
+  // const rateNeutral = () => {
+  //   console.log('Neutral value before', neutral)
+  //   setNeutral(neutral + 1)
+  // }
+  // const rateBad = () => {
+  //   console.log('Bad value before', bad)
+  //   setBad(bad + 1)
+  // }
+  const rateNeutral = () => setNeutral(neutral + 1)
   const rateBad = () => setBad(bad + 1)
 
 
   return (
     <>
       <h1>Give Feedback</h1>
-      <Button 
+      <Button
         handleClick={rateGood}
         text='Good'
       />
+      <Button
+        handleClick={rateNeutral}
+        text='Neutral'
+      />
+      <Button
+        handleClick={rateBad}
+        text='Bad'
+      />
       <h1>Statistics</h1>
-      <Display 
-        value={good}
-        text='Good'
-        />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </>
   )
 }
