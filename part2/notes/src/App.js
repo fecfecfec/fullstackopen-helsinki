cc3007da8e2695dd6e72e9a3dcd28af2910f2acd
Francisco M. Ferreiro <fechex@gmail.com>
2023-07-19 18:12:22 -0300

    Added notes practice

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,14 +1,54 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('Add a note...')
+  const [showAll, setShowAll] = useState(true) 
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  // This uses the conditional operator if noteToShow is True (in this case showAll value is true by default), then all notes are shown, in case it false, then only the important notes are shown.
+  // ': notes.filter(note => note.important === true)' can be reduced to 'notes.filter(note => note.important)' since, as showAll, note.important is either true or false. Without declaring it's going to check if it's true
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'Important' : 'All' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">Save</button>
+      </form>  
     </>
   )
 }
