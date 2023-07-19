f79f5f84c6b5b955e98d7e9b8cc3f8622929b943
Francisco M. Ferreiro <fechex@gmail.com>
2023-07-18 22:35:55 -0300

    Fixed Unicafe app naming

--- part1/src/index-unicafe.js
+++ part1/src/index-unicafe.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import Unicafe from './unicafe'
+import Unicafe from './AppUnicafe'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Unicafe />)
