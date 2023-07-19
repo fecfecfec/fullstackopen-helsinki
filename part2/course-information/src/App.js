44ecb47c6174c56c9624770dff8af77dc475b88a
Francisco M. Ferreiro <fechex@gmail.com>
2023-07-19 16:13:42 -0300

    Clean up

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -29,9 +29,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  // const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises => Verbose
   const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
-  console.log(total)
   return (
     <>
       <p><strong>Number of exercises: {total}</strong></p>
