1e2f31a4fdb318734b448ca0845e5cabbcb216e0
Francisco M. Ferreiro <fechex@gmail.com>
2023-07-18 22:33:50 -0300

    Adjustments in index.

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import Unicafe from './Unicafe'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Unicafe />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
