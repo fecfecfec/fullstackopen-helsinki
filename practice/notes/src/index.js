93432df772736446b9441658f8a9e8ca578b6615
Francisco M. Ferreiro <fechex@gmail.com>
2023-07-20 10:12:29 -0300

    Axios and promises practice.

--- practice/notes/src/index.js
+++ practice/notes/src/index.js
@@ -1,26 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
 import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+// const promise = axios.get('http://localhost:3001/notes')
+// promise.then(response => {
+//   console.log(response)
+// })
+
+axios
+.get('http://localhost:3001/notes')
+.then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App notes={notes} />
+  )
+})
+
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// promise2.then(response => {
+//   console.log(response)
+// })
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true
+//   }
+// ]
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <App notes={notes} />
+// )
